3bced9333e64187aae02c4cc3e93184bf0224a70
Gabriel Saiz <107472727+GabriLPDA22@users.noreply.github.com>
2024-02-16 18:42:41 +0100

    Update Inmueble.java

--- Objetos/Herencia/ClasePricipal/Inmueble.java
+++ Objetos/Herencia/ClasePricipal/Inmueble.java
@@ -1,6 +1,6 @@
 package Objetos.Herencia.ClasePricipal;
 
-public abstract class Inmueble {
+public class Inmueble {
     protected String direccion;
     protected double metrosCuadrados;
     protected int antiguedad; // Años de antigüedad del inmueble
