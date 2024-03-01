a32d27acf667b2244a0601d4834d2bf6b6c0573c
Gabriel Saiz <107472727+GabriLPDA22@users.noreply.github.com>
2024-03-01 19:53:36 +0100

    añadido enunciado

--- Excepciones/Exc2.java
+++ Excepciones/Exc2.java
@@ -1,5 +1,10 @@
 package Excepciones;
 
+//2.  Implementa un programa que pida dos valores int A y B utilizando un nextInt() (de Scanner), calcule A/B y muestre el resultado por pantalla. 
+//    Se deberán tratar de forma independiente las dos posibles excepciones, 
+//    InputMismatchException y ArithmeticException, mostrando en cada caso un mensaje de error diferente en cada caso.
+
+
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
